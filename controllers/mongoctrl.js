380dcb8c59b8023f8c8809d653cf541a2253a576
marissaconner <marissafromtexas@gmail.com>
2020-05-24 10:48:10 -0700

    Extracting rows in a batch w fast-csv options

--- controllers/mongoctrl.js
+++ controllers/mongoctrl.js
@@ -2,6 +2,8 @@ const path = require('path');
 // load whatever we need to connect with
 // load whatever we need as a schema file 
 const { MongoClient } = require('mongodb');
+const fs = require('fs');
+const fastcsv = require('fast-csv');
 
 const port = process.env.MONGOPORT || 27017;
 const user = process.env.DBUSER || 'productservice';
@@ -23,10 +25,37 @@ const MongoController = {
   },
 
   connectAndSeed: function(client) {
-    // connect to the client 
-    console.log( "connect and seed is triggered.")
+    console.log( "connect and seed is triggered.");
+        // connect to the client , do 1k per batch.
+    let dataToWrite = [];
+    let stream = fs.createReadStream(`${filepath}/cities.csv`);
+
+    const parseRows = function(batch){
+      stream.pipe(fastcsv.parse({ skipRows: batch, maxRows: batch }))
+        .on('error', (error) => {
+          console.error(error);
+        })
+        .on('data', (data) => {
+          const row = {
+            id: data[0],
+            name: data[1],
+            governing_district: data[2],
+            country: data[3],
+            latitude: data[4],
+            longitude: data[5],
+            postal_code: data[6]
+          };
+          dataToWrite.push(row);
+        })
+        .on('end', rowCount => {
+          console.log(`Parsed ${rowCount} rows from ${batch}`);
+        });
+    }
+
+    for( var i = 0; i < 3000; i += 1000 ) {
+      parseRows(i);
+    }
   }
-};
+}
 
 module.exports = MongoController; 
-
