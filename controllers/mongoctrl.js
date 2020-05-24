1f37a540940a544dcc46a7308d61f6bc64b11038
marissaconner <marissafromtexas@gmail.com>
2020-05-24 13:12:35 -0700

    Stream that reads through 10M lines of a CSV

--- controllers/mongoctrl.js
+++ controllers/mongoctrl.js
@@ -3,7 +3,7 @@ const path = require('path');
 // load whatever we need as a schema file 
 const { MongoClient } = require('mongodb');
 const fs = require('fs');
-const fastcsv = require('fast-csv');
+const csv = require('csv-stream');
 
 const port = process.env.MONGOPORT || 27017;
 const user = process.env.DBUSER || 'productservice';
@@ -28,10 +28,34 @@ const MongoController = {
     console.log( "connect and seed is triggered.");
         // connect to the client , do 1k per batch.
     let dataToWrite = [];
-    let stream = fs.createReadStream(`${filepath}/cities.csv`);
+    let stream = fs.createReadStream(`${filepath}/products.csv`);
+    let csvStream = csv.createStream();
 
+    stream.pipe(csvStream)
+     .on('error', (error) => {
+          console.error(error);
+        })
+      .on('data', (data) => {
+        const row = {
+          id: data[0],
+          name: data[1],
+          governing_district: data[2],
+          country: data[3],
+          latitude: data[4],
+          longitude: data[5],
+          postal_code: data[6]
+        };
+        dataToWrite.push(row);
+        console.log( data.id );
+      })
+      .on('end', (rowCount) => {
+        console.log(`Parsed ${rowCount} rows`);
+      });
+
+    /*
+    let stream = fs.createReadStream(`${filepath}/products.csv`);
     const parseRows = function(batch){
-      stream.pipe(fastcsv.parse({ skipRows: batch, maxRows: batch }))
+      stream.pipe(fastcsv.parse({ skipRows: batch, maxRows: 1000 }))
         .on('error', (error) => {
           console.error(error);
         })
@@ -51,10 +75,10 @@ const MongoController = {
           console.log(`Parsed ${rowCount} rows from ${batch}`);
         });
     }
-
-    for( var i = 0; i < 3000; i += 1000 ) {
+    for( var i = 0; i < 10000000; i += 1000 ) {
       parseRows(i);
     }
+    */
   }
 }
 
