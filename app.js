f1167e03a70e39ceb2476e2ccbfc0c7bbb74dfb8
marissaconner <marissafromtexas@gmail.com>
2020-05-24 18:15:12 -0700

    script to load 10M rows of product

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const serverport = process.env.PORT || 3000;
 const db = require('./controllers/mongoctrl.js');
 
 const app = express();
-let client = null;
-db.createClient( function(connection) {
-  client = connection;
-  db.connectAndSeed(client);
-});
+let client = db.createClient();
+db.connectAndSeed(client);
 
 var server = app.listen(serverport, function(){
   console.log( `Server running on ${serverport}!!`);
 });
+
+/* Perhaps you have started the http server before making connection with mongodb server. That's why the variable db was undefined when your post api was loaded in the memory. Since the connection with mongodb is async. In order to utilize the mongodb instance you have to first connect with mongdb server and then starts the http server and store the db instance globally or in some module which you can require and get the db instance to perform queries.*/
