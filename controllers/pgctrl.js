395d5a306c2fe4665e414a3d66523d1e761764d6
marissaconner <marissafromtexas@gmail.com>
2020-05-28 14:50:51 -0700

    Script goes through the motions of parsing the products

--- controllers/pgctrl.js
+++ controllers/pgctrl.js
@@ -44,6 +44,41 @@ const PGController = {
       )
         .then(()=>{
           console.log('Primary records loaded.')
+          let csvStream = csv.createStream();
+          var count = 0;
+          var queue = []
+          
+          const bulkWrite = function(operations, callback) {
+            //perform the bulkwrite.
+            callback(null)
+          }
+
+          const prepareInventoryForWrite = function(id){
+            count++;
+            //make the inventory object.
+          }
+
+          var stream = fs.createReadStream(`${filepath}/products.csv`);
+          stream.pipe(csvStream)
+            .on('end', (data)=>{
+              console.log('10M rows processed');
+            })
+            .on('data', (data) => {
+              count++;
+              prepareInventoryForWrite(data.id);
+              if( count >= 1000 ) {
+                stream.pause()
+                bulkWrite(queue, (err) => {
+                  if(err) {
+                    console.log(err);
+                    return;
+                  }
+                  console.log('Bulkwrote');
+                  count = 0; 
+                  stream.resume();
+                })
+              } 
+          })
         })
         .catch((err)=>{
           console.error(err);
