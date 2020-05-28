3961bb27a5bd830ac9f6fa9c439e939820910838
marissaconner <marissafromtexas@gmail.com>
2020-05-28 14:40:43 -0700

    Connection and primary records made through pg-promise

--- controllers/pgctrl.js
+++ controllers/pgctrl.js
@@ -17,102 +17,40 @@ const filepath = path.join(__dirname, '..' , 'lib', 'csv');
 const PGController = {
 
   createClient: function() {
-    return pgp(`postgres://localhost:${pgport}/${database}`);
-    // return new Client({
-    //   user,
-    //   password,
-    //   host,
-    //   port,
-    //   database
-    // });
+    console.log('CreateClient running.');
+    const connectionDetails = `postgres://${user}:${password}@${host}:${port}/${database}`;
+    const db = pgp(connectionDetails);
+    return db;
+
   },
 
   connectAndSeed: function(client) {
-    client.connect()
-    .then(() => {
-      console.log( `Postgres connection established on port ${port}.`);
-      client.query(loadSchema)
-      .then(() => {
-        console.log( 'Schema successfully loaded.');
-        client.query(
+    client.none(loadSchema)
+    .then(()=>{
+      console.log('Schema loaded.');
+      client.none(
+      `COPY products
+        FROM '${filepath}/products.csv'
+        WITH (format csv, header);
 
-          `COPY products
-          FROM '${filepath}/products.csv'
-          WITH (format csv, header);
+        COPY stores
+        FROM '${filepath}/stores.csv'
+        WITH (format csv, header);
 
-          COPY stores
-          FROM '${filepath}/stores.csv'
-          WITH (format csv, header);
-
-          COPY locations
-          FROM '${filepath}/cities.csv'
-          WITH (format csv, header);
-          `
-        )
+        COPY locations
+        FROM '${filepath}/cities.csv'
+        WITH (format csv, header);
+            `
+      )
         .then(()=>{
-
-          console.log('Primary data loaded.')
-          const pgp = pgp(client);
-
-          let csvStream = csv.createStream();
-
-          var count = 0;
-          var queue = []
-          let supercounter = 0; 
-
-          const bulkWrite = function(operations, callback) {
-            while (operations.length > 0 ) {
-              const op = operations.pop();
-              // client.query(op)
-              // .catch((err) => {
-              //   callback(error)
-              // })
-            }
-            callback(null)
-          }
-
-          const prepareInventoryForWrite = function(id){
-            count++;
-            let values = '';
-            for( let i = 0; i < 15; i += 1 ) {
-              values += `(${id}, ${rand1k()}),`;
-            }
-            values += `(${id}, ${rand1k()})`;
-            let query = `INSERT INTO products_stores (product_id, store_id) VALUES ${values} RETURNING ID`;
-            queue.push(query);
-          }
-
-          var stream = fs.createReadStream(`${filepath}/products.csv`);
-          stream.pipe(csvStream)
-            .on('end', (data)=>{
-              console.log('10M rows processed');
-            })
-            .on('data', (data) => {
-              count++;
-              console.log(data);
-              console.log('chunk');
-              prepareInventoryForWrite(data.id);
-              if( count >= 1000 ) {
-                stream.pause()
-                bulkWrite(queue, (err) => {
-                  if(err) {
-                    console.log(err);
-                    return;
-                  }
-                  console.log('Bulkwrote');
-                  count = 0; 
-                  stream.resume();
-                })
-              } 
-          })
-
-
-       
-          
+          console.log('Primary records loaded.')
         })
-      })
+        .catch((err)=>{
+          console.error(err);
+        })
     })
-  }
-}
+    
+      }
+    }
 
 module.exports = PGController; 
